5dfdaf72d1da91716b4c6ec30438ad3d95394f48
Derek <6804504+derekjc@users.noreply.github.com>
2021-02-12 05:48:20 +0000

    Update setup and fix requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='requestbin',
-    version='2.0.0',
+    version='3.0.0',
     author='Runscope',
     author_email='requestbin@runscope.com',
     description='HTTP request collector and inspector',
     packages=find_packages(),
-    install_requires=['feedparser'],
+    install_requires=['feedparser', 'Flask'],
+    python_requires='~=3.5',
     data_files=[],
 )
